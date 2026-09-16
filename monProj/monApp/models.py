3b2563f3ee21c989bfb5ef82562e4bafc8fca856
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2026-09-16 16:43:47 +0200

    Création du modèle

--- monProj/monApp/models.py
+++ monProj/monApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    idCat = models.AutoField(primary_key=True)
+    nomCat = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nomCat
+
+
+class Produit(models.Model):
+    refProd = models.AutoField(primary_key=True)
+    intituleProd = models.CharField(max_length=200)
+    prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
+    # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie 1,1 côté produit)→
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produit_categorie",null=True,
+blank=True)
+
+def __str__(self):
+    return self.intituleProd
