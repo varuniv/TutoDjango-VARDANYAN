b7559c9c79ab4b9b88ccd8ad24253cfaa440c464
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2026-09-16 16:19:52 +0200

    création des url et des views

--- monProj/monApp/urls.py
+++ monProj/monApp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("home/<param>", views.home, name="home"),    
+    path("contactus", views.contactus, name="contactus"),
+    path("aboutus", views.aboutus, name="aboutus"),
 ]
--- monProj/monApp/views.py
+++ monProj/monApp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import HttpResponse 
 
-def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+def home(request,param=""):
+    return HttpResponse("<h1>Bonjour " + param + " !<h2>")
+
+def contactus(request):
+    return HttpResponse("<h1>Contact Us</h1>")
+
+def aboutus(request):
+    return HttpResponse("<h1>Something about us</h1>")
